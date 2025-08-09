532958e2a532a579b4962e7830e6e028aad9020c
albert <albertgassol1@gmail.com>
2025-08-09 19:44:46 +0200

    Fix path on setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 cd
 python3 -m venv vlm_server_env
-vlm_server_env/bin/python -m pip install -r semantic_inference_python/models/deepseek/requirements.txt
+vlm_server_env/bin/python -m pip install -r deepseek/requirements.txt
